e2495e19affda8d22b048e05239143c070a57122
Anubhav Kumar <a.k.gupta159753@gmail.com>
2024-06-21 20:26:53 +0530

    Added echo Names of girls

---
+++ loop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Names of Girls"
+declare -a Names=(Poornima Rupali Ritika Pallavi)
+arrLength=${#Names[@]}
+echo "Number of girls $arrLength"
+for((i=0;i<arrLength;i++)){
+    echo "Girl Number ${i+1} :- ${Names[i]} has ${#Names[i]} letters in her name"
+}
